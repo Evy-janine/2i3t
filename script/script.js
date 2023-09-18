5a27529b730f356416314c0d03c673ee752145f7
Evy-janine <108942363+Evy-janine@users.noreply.github.com>
2023-09-18 10:52:54 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,3 +1,35 @@
-function alo_mundo () {
-    alert("Projeto Iniciado! Alô!")
+escrever = msg => alert(msg);
+soma =(a,b) => a + b;
+sub =(a,b) => a - b;
+mult = (a,b) => a * b; 
+div = (a,b) => a / b;
+raiz = a => Math.sqrt(a);
+equacao2Grau = (a,b,c) =>{
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if(delta < 0) return "Não possui raiz Real.";
+    if(delta == 0) return "x1 = x2 =" + div(-b,mult(2,a));
+    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) +
+           " x2 = " +  div(sub(-b,raiz(delta)),mult(2,a)); 
+}
+
+functin mostrar_resultado(){
+    const set_resultado = document.getElementById("resultado").value = valor;
+
+}
+function calcular(){
+    if(executar != ""){
+    if(executar = "soma") mostrar_resultado(soma(a,b));
+    if(executar = "sub") mostrar_resultado(sub(a,b));
+    if(executar = "div") mostrar_resultado(div(a,b));
+    if(executar = "mult") mostrar_resultado(mult(a,b));
+    executar = "";
+    }
+}
+function digitando(tecla){
+   if(executar == "") a += tecla;
+    a += tecla;
+    alert(a);
+}else{
+    b += tecla;  
+  
 }
