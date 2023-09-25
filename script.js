7af716ef54bac7e811627eedfbfe1bc68158997f
Raidmello <108942162+Raidmello@users.noreply.github.com>
2023-09-25 10:47:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,35 @@
-let a = "";
-let b = "";
-let valor = 0;
-let executar = "";
-
-soma = (a,b) => a + b;
-sub = (a,b) => a - b;
-mult = (a,b) => a * b;
+escrever = msg => alert(msg);
+soma =(a,b) => a + b;
+sub =(a,b) => a - b;
+mult = (a,b) => a * b; 
 div = (a,b) => a / b;
 raiz = a => Math.sqrt(a);
 equacao2Grau = (a,b,c) =>{
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
-    if (delta < 0) return "Não possui raiz Real.";
-    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
-    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) + 
-           " x2 = " + div(sub(-b,raiz(delta)),mult(2,a));
+    if(delta < 0) return "Não possui raiz Real.";
+    if(delta == 0) return "x1 = x2 =" + div(-b,mult(2,a));
+    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) +
+           " x2 = " +  div(sub(-b,raiz(delta)),mult(2,a)); 
 }
-function mostrar_resultado(){
-    document.getElementById("resultado").value = valor;
+
+functin mostrar_resultado(){
+    const set_resultado = document.getElementById("resultado").value = valor;
+
 }
 function calcular(){
     if(executar != ""){
-        b = valor; 
-        if(executar = "soma")valor = soma(a,b);
-        if(executar = "sub")valor = sub(a,b);
-        if(executar = "div")valor = div(a,b);
-        if(executar = "mult")valor = mult(a,b);
-        mostrar_resultado();
-        executar = "";
-        a = ""
-        b = "";
-    }
-    function operacao(op){
-        executar = op;
-        a = valor;
-        valor = "";
+    if(executar = "soma") mostrar_resultado(soma(a,b));
+    if(executar = "sub") mostrar_resultado(sub(a,b));
+    if(executar = "div") mostrar_resultado(div(a,b));
+    if(executar = "mult") mostrar_resultado(mult(a,b));
+    executar = "";
     }
 }
 function digitando(tecla){
-   if(tecla == "."){
-       if(!temPonto){
-        valor = valor + tecla ;
-        mostrar_resultado();
-        temPonto = true ;
-       }
-       return;
-   } 
-   valor = valor + tecla;
-   mostrar_resultado();  
+   if(executar == "") a += tecla;
+    a += tecla;
+    alert(a);
+}else{
+    b += tecla;  
+  
 }
