a6257e525d833c58dfdfd53766fdd99ff2d49468
Evy-janine <108942363+Evy-janine@users.noreply.github.com>
2023-11-06 10:52:19 -0300

    Add files via upload

--- avaliacao/script.js
+++ avaliacao/script.js
@@ -1,84 +1,66 @@
-let lista = ["Evelyn","Raul"];
-const atualiza_lista = () =>{
-    let mostrar = "Lista Vazia";
-    console.log(lista)
-    if(lista.length > 0){   
-        mostrar = "";
-        for(let i = 0; i < lista.length; i++ ){
-            mostrar += lista[i] + "<br>";
+let lista_nome = ["Evelyn","Raul"];
+function atualizar_lista(){
+    let nomes = "";
+    if(lista_nome.length > 0){
+        for(let i=0; i < lista_nome.length; i++){
+            nomes += lista_nome[i] + "<br>";
         }
+        document.getElementById("lista").innerHTML = nomes;
+        return;
     }
-    document.getElementById("lista").innerHTML = mostrar;
+    document.getElementById("lista").innerHTML = "Lista Vazia";
 }
-const pegar_nome = () => validar_nome(document.getElementById("nome").value);
-const mostra_erro = (erro) =>  document.getElementById("error").innerHTML = erro;
-const validar_nome = (nome) => {
-    mostra_erro("");
-    for(let i = 0; i < lista.length; i++ ){
-        if(lista[i].toUpperCase() == nome.toUpperCase()){
-            mostra_erro("Nome já está na lista");
-            return "";
+
+function inserir_ultimo() {
+    let nome = document.getElementById("nome").value;
+    if(nome != ""){
+        lista_nome.push(nome);
+        atualizar_lista();
+    }
+}
+function inserir_primeiro() {
+    let nome = document.getElementById("nome").value;
+    if(nome != ""){
+        lista_nome.unshift(nome);
+        atualizar_lista();
+    }
+}
+function deletar_primeiro() {
+    if(lista_nome.length > 0){
+        lista_nome.shift();
+        atualizar_lista();
+    }
+}
+function deletar_ultimo() {
+    if(lista_nome.length > 0){
+        lista_nome.pop();
+        atualizar_lista();
+    }
+}
+function deletar_lista() {
+    lista_nome = [];
+    atualizar_lista();
+}
+function ordenar_az() {
+    if(lista_nome.length > 0){
+        lista_nome.sort();
+        atualizar_lista();
+    }
+}
+function ordenar_za() {
+    if(lista_nome.length > 0){
+        lista_nome.sort();
+        lista_nome.reverse();
+        atualizar_lista();
+    }
+}
+function deletar_nome(){
+    let nome = document.getElementById("nome").value;
+    if(nome != ""){
+        for(let i=0; i < lista_nome.length;i++){
+            if(nome == lista_nome[i]) lista_nome.splice(i,1);
         }
+        atualizar_lista();
     }
-    document.getElementById("nome").value = "";
-    return nome;
-}
 
-//Faça os botões no minha_lista.html e a função neste arquivo js.
-// inserir_primeiro  deletar_ultimo  deletar_primeiro deletar_nome
-// O deletar_nome deverá deletar o nome digitado, na posição que quiser.
-// limpar_lista, deverá apagar toda a lista.
-// odernar_az deverá ordenar em ordem alfabética crescente
-// ordenar_za deverá ordenar em ordem alfabética decrescente
-
-
-
-const inserir_ultimo = () => {
-    let nome = pegar_nome();
-    if( nome != ""){
-        lista.push(nome);
-        atualiza_lista();
-    } 
-}
-const inserir_primeiro = () => {
-    let nome = pegar_nome();
-    if( nome != ""){
-        lista.unshift(nome);
-        atualiza_lista();
-    } 
-}
-const deletar_primeiro = () => {
-    if( lista.length > 0){
-        lista.shift();
-        atualiza_lista();
-    } 
-}
-const deletar_ultimo = () => {
-    if( lista.length > 0){
-        lista.pop();
-        atualiza_lista();
-    } 
-}
-const limpar_lista = () => {
-        lista = [];
-        atualiza_lista();
-    
-}
-const ordenar_az = () => {
-    if( lista.length > 0){
-        lista.sort();
-        atualiza_lista();
-    } 
-}
-const ordenar_za = () => {
-    if( lista.length > 0){
-        lista.reverse();
-        atualiza_lista();
-    } 
-}
-const remover_nome = () => {
-    if( lista.length > 0){
-        lista.pop();
-        atualiza_lista();
-    } 
 }
--- script.js
+++ script.js
@@ -1,35 +1,136 @@
-escrever = msg => alert(msg);
-soma =(a,b) => a + b;
-sub =(a,b) => a - b;
-mult = (a,b) => a * b; 
-div = (a,b) => a / b;
+let a = "";
+let b = "";
+let c = "";
+let sa = "+";
+let sb = "+";
+let sc = "+";
+let valor = "";
+let executar = "";
+let temPonto = false;
+let desligada = false;
+
+soma = (a,b) => Number(a) + Number(b);
+sub = (a,b) => Number(a) - Number(b);
+mult = (a,b) => Number(a) * Number(b);
+div = (a,b) => Number(a) / Number(b);
 raiz = a => Math.sqrt(a);
+
 equacao2Grau = (a,b,c) =>{
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
-    if(delta < 0) return "Não possui raiz Real.";
-    if(delta == 0) return "x1 = x2 =" + div(-b,mult(2,a));
-    return "x1 = " + div(soma(-b,raiz(delta)),mult(2,a)) +
-           " x2 = " +  div(sub(-b,raiz(delta)),mult(2,a)); 
+    document.getElementById("delta").innerHTML = delta;
+    
+    if (delta < 0) return "Não possui raiz Real.";
+    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x<sub>1</sub> = " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+           " x2 = " + div(sub(-b,raiz(delta)),mult(2,a));
 }
-
-functin mostrar_resultado(){
-    const set_resultado = document.getElementById("resultado").value = valor;
-
+function mostrar_resultado(){
+    document.getElementById("resultado").value = valor;
 }
 function calcular(){
+    if (desligada) return;
     if(executar != ""){
-    if(executar = "soma") mostrar_resultado(soma(a,b));
-    if(executar = "sub") mostrar_resultado(sub(a,b));
-    if(executar = "div") mostrar_resultado(div(a,b));
-    if(executar = "mult") mostrar_resultado(mult(a,b));
-    executar = "";
+        b = valor;
+        if(executar == "soma") valor = soma(a,b);
+        if(executar == "sub") valor = sub(a,b);
+        if(executar == "div") valor = div(a,b);
+        if(executar == "mult") valor = mult(a,b);
+        if(executar == "porc") valor = mult(div(a/100),b);
+        if(executar == "raiz") valor = raiz(a);
+
+        mostrar_resultado();
+        executar = "";
+        a = ""
+        b = "";
+        valor = "";
+        temPonto = false;
     }
 }
+function desliga(){
+    desligada = !desligada;
+    if(desligada){
+        zerar();
+    }else{
+       zerar();
+        mostrar_resultado();
+    }
+    return desligada;
+}
+desliga();
+function calcular_raiz(){
+    if(valor == ""){
+        valor == 0;
+    }
+    valor = raiz(valor);
+    mostrar_resultado();
+}
+function porcentagem(){
+    if(executar == "mult"){
+        b = valor;
+        valor = div (mult(a,b),100);
+        mostrar_resultado();
+    }
+}
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    valor = "0";
+    executar = "";
+        mostrar_resultado();
+     valor = "";
+}
+    function operacao(op){
+        if(desligada) return;
+            executar = op;
+            a = valor;
+            valor = "";
+    }
 function digitando(tecla){
-   if(executar == "") a += tecla;
-    a += tecla;
-    alert(a);
-}else{
-    b += tecla;  
-  
+    if(tecla == "."){
+        if(!temPonto){
+            mostrar_resultado();
+         temPonto = true;
+        }
+        return;
+    }
+  valor = valor + tecla;
+  mostrar_resultado();
+}
+const caclcular_equacao = () =>{
+    alert(" " + a + "b" + b)
+    if (a != "" && a != "0")
+    a = Number(a);
+    b = Number(a);
+    c = Number(a);
+    if(sa != "+") a = -a;
+    if(sb != "+") a = -b;
+    if(sc != "+") a = -c;
+    alert(sa + a + "x2" + sb + b + "x" + sc + c + "= 0")
+    document.getElementById("raiz").innerHTML = equacao2Grau(a,b,c);
+}
+   
+const set_sinal_a = () =>{
+    sa = document.getElementById("sinal_sa").value;
+    caclcular_equacao();
+}
+const set_sinal_b = () =>{
+    sb = document.getElementById("sinal_sb").value;
+    caclcular_equacao();
+}
+const set_sinal_c = () =>{
+    c = document.getElementById("sinal_sc").value;
+    caclcular_equacao();
+}
+const set_valor_a = () =>{
+    a = document.getElementById("valor_a").value;
+    caclcular_equacao();
+}
+const set_valor_b = () =>{
+    a = document.getElementById("valor_b").value;
+    caclcular_equacao();
+}
+const set_valor_c = () =>{
+    a = document.getElementById("valor_c").value;
+    caclcular_equacao();
 }
